ac1e277ee9fb35004faa4e712a8f684f2fb089ec
joshnimaryjohn-art <joshnimaryjohn@gmail.com>
2026-01-16 21:20:55 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,11 @@
 function openPopup(id) {
   const overlay = document.getElementById('overlay');
   const popups = document.querySelectorAll('.popup');
-
   overlay.style.display = 'flex';
   popups.forEach(p => p.style.display = 'none');
-
   const active = document.getElementById(id);
   active.style.display = 'block';
   active.querySelector('.popup-content').scrollTop = 0;
-
-}
-function openImagePopup(src) {
-  const popup = document.getElementById('imagePopup');
-  const img = document.getElementById('fullImage');
-  img.src = src;
-  popup.style.display = 'flex';
-}
-document.getElementById('imagePopup').addEventListener('click', (e) => {
-  if (e.target.id === 'imagePopup') closeImagePopup();
-});
-
-
-function closeImagePopup() {
-  document.getElementById('imagePopup').style.display = 'none';
 }
 
 function closePopup() {
@@ -34,6 +17,23 @@ function scrollPopup(id, amount) {
   const content = popup.querySelector('.popup-content');
   content.scrollBy({ top: amount, behavior: 'smooth' });
 }
+
+function openImagePopup(src) {
+  const popup = document.getElementById('imagePopup');
+  const img = document.getElementById('fullImage');
+  img.src = src;
+  popup.style.display = 'flex';
+}
+
+function closeImagePopup() {
+  document.getElementById('imagePopup').style.display = 'none';
+}
+
+/* CLOSE ON OUTSIDE CLICK */
 document.getElementById('overlay').addEventListener('click', (e) => {
   if (e.target.id === 'overlay') closePopup();
+});
+
+document.getElementById('imagePopup').addEventListener('click', (e) => {
+  if (e.target.id === 'imagePopup') closeImagePopup();
 });
